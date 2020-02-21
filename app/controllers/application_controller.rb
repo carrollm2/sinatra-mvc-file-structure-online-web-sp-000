7c1be2b64dd667f5b5049f1f39576f32f3db8d02
Michael Carroll <carrollm2@gmail.com>
2020-02-21 02:59:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    class Dog
+
+      attr_accessor :name, :breed, :age
+
+      def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+      end
+    end
   	erb :index
   end
 
